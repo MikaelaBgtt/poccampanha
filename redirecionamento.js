32e8ff5684f04968f82df89a70b03e860f9a3faa
Mikaéla <mikaela.cardoso@fatec.sp.gov.br>
2023-08-07 16:59:46 -0300

    atualização

--- redirecionamento.js
+++ redirecionamento.js
@@ -1,34 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
+function navigateToNextURL() {
   const urlParams = new URLSearchParams(window.location.search);
   const nomeCampanha = urlParams.get('nome');
-  const qtdeCliquesMax = urlParams.get('cliques')
-  const links = urlParams.get('links')
-  array = links.split(',')
+  const qtdeCliquesMax = parseInt(urlParams.get('cliques'));
+  const links = urlParams.get('links');
+  const array = links.split(',');
+
+  let qtdeCliques = parseInt(localStorage.getItem('cliques'));
+  if (isNaN(qtdeCliques) || qtdeCliques < 0) {
+    qtdeCliques = 0;
+  }
+
   const nomeCampanhaExibido = document.getElementById("nomeCampanhaExibido");
   if (nomeCampanha) {
     nomeCampanhaExibido.textContent = "Nome da campanha digitado: " + decodeURIComponent(nomeCampanha);
   } else {
     nomeCampanhaExibido.textContent = "Nenhum nome de campanha foi digitado!";
   }
-  let qtdeCliques = localStorage.getItem('cliques');
 
-      if (!qtdeCliques) {
-        qtdeCliques = 1;
-      } else {
-        qtdeCliques = parseInt(qtdeCliques) + 1;
-        
-      }
-      localStorage.setItem('cliques', qtdeCliques);
-      const qtdeCliquesExibida = document.getElementById("qtdeCliquesExibida");
-      qtdeCliquesExibida.textContent = "Quantidade de cliques: " + qtdeCliques;
-      if (parseInt(qtdeCliques) > qtdeCliquesMax) {
-        const ultimoIndice = array.length - 1;
-        window.location.href = array[ultimoIndice];
-      } else {
-        const proximoIndice = parseInt(qtdeCliques) % array.length;
-        window.location.href = array[proximoIndice];
-      }
-    });
+  const qtdeCliquesExibida = document.getElementById("qtdeCliquesExibida");
+  qtdeCliquesExibida.textContent = "Quantidade de cliques: " + qtdeCliques;
+
+  if (qtdeCliques >= qtdeCliquesMax + 1) {
+    localStorage.setItem('cliques', 0);
+    window.location.href = array[0] + `?nome=${encodeURIComponent(nomeCampanha)}&cliques=${qtdeCliquesMax}&links=${links}`;
+  } else {
+    const proximoIndice = qtdeCliques % array.length;
+    window.location.href = array[proximoIndice] + `?nome=${encodeURIComponent(nomeCampanha)}&cliques=${qtdeCliques + 1}&links=${links}`;
+    localStorage.setItem('cliques', qtdeCliques + 1);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", navigateToNextURL);
 
     function getParameterByName(name) {
       const urlParams = new URLSearchParams(window.location.search);
